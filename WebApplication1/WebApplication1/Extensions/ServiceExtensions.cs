1c2fee83a7372cf8ea7e433314035f56c71a33f0
Dianayepesc <114169740+Dianayepesc@users.noreply.github.com>
2023-07-24 17:16:34 -0500

    modificacion

---
+++ WebApplication1/WebApplication1/Extensions/ServiceExtensions.cs
@@ -0,0 +1,17 @@
+﻿namespace WebApplication1.Extensions
+{
+    public class ServiceExtensions
+    {
+        public static void ConfigureCors(this IServiceCollection services) => services.AddCors(options =>
+        {
+            options.AddPolicy("CorsPolicy", builder => builder
+
+
+
+            .AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader());
+
+        });
+    }
+}
